4e44963de9c8fe61c4c98aadbcf87091a214a3cf
varsha.zurange@inflectionzone.com <varsha.zurange@inflectionzone.com>
2023-06-03 17:05:12 +0530

    Create multiple method added in appointment status, and made some changes in create, update methods

--- src/api/appointment.status/appointment.status.routes.ts
+++ src/api/appointment.status/appointment.status.routes.ts
@@ -11,10 +11,11 @@ export const register = (app: express.Application): void => {
     const controller = new AppointmentStatusController();
 
     router.post('', controller.create);
-    router.put('/:id', authenticator.authenticateClient,controller.update);
-    router.delete('/:id', authenticator.authenticateClient, controller.delete);
+    router.post('/add-multiple', controller.createMultiple);
     router.get('/search', authenticator.authenticateClient, controller.search);
     router.get('/:id', authenticator.authenticateClient, controller.getById);
-     
+    router.put('/:id', authenticator.authenticateClient,controller.update);
+    router.delete('/:id', authenticator.authenticateClient, controller.delete);
+    
     app.use('/api/v1/appointment-statuses', router);
 };
